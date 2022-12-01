adcf9507cdbede12ed01ad759cbfebebd5e1c9d8
Josh011600 <114022933+Josh011600@users.noreply.github.com>
2022-12-01 09:52:23 +0800

    Add files via upload

---
+++ crudoperations/user.php
@@ -0,0 +1,41 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <meta name="description" content="">
+ <meta name="author" content="">
+ <title>CRUD Operations </title>
+
+ <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1> Hello World!</h1>
+  
+  <form>
+  <table>
+    <tr>
+      <td>
+        Email Address:
+      </td>
+
+      <td>
+        <input type=”mail”  placeholder=”Email” name=””>
+      </td>
+    </tr>
+<tr>
+<td>
+        Password:
+      </td>
+
+      <td>
+        <input type=”Password” placeholder=”Password” name=””>
+      </td>
+    </tr>
+  </table>
+</form>          
+       
+ </body>
+</html>
